a4d45cacf6327c4902e1e4bf8c55dfd25a44188d
Olayinka Ibrahim <olayinka.ibrahim@etranzact.com>
2020-11-05 23:43:33 +0100

    change review entries component

--- src/components/Forms/SummaryPage.js
+++ src/components/Forms/SummaryPage.js
@@ -1,6 +1,7 @@
 import React,{Fragment} from 'react'
 import { makeStyles } from '@material-ui/core/styles';
 import { useFormikContext } from 'formik';
+import moment from 'moment';
 import {
     Typography,
     Paper,
@@ -19,21 +20,146 @@ const useStyles = makeStyles({
   });
 
 
-const getValue = (value)=>{
-    if(value===true){
-        return "YES"
-    } else if(value===false){
-        return "NO"
-    } else {
-        return value
-    }
-}
+// const getValue = (value)=>{
+//     if(value===true){
+//         return "YES"
+//     } else if(value===false){
+//         return "NO"
+//     } else {
+//         return value
+//     }
+// }
 
 const SummaryPage = ({stepTitle,activeStep}) => {
     const classes = useStyles();
     const { values } = useFormikContext();
+    const {
+        prefix,
+        firstName,
+        lastName,
+        middleName,
+        suffix,
+        sex,
+        dateOfBirth,
+        placeOfBirth,
+        address,
+        aptSuite,
+        city,
+        stateProvince,
+        postalCode,
+        emailAddress,
+        credentialType,
+        credentialNumber,
+        dateOfIssue,
+        dateOfExpiration,
+        issuingAuthority,
+        sendMedium,
+        discountOffer
+    } = values
     return (
         <Fragment>
+            <Typography variant="h6" gutterBottom align="center">
+                {stepTitle[activeStep]}
+            </Typography>
+            <TableContainer component={Paper}>
+                <Table className={classes.table} aria-label="simple table">
+                    <TableBody>
+                            <TableRow>
+                                <TableCell align="left">Prefix</TableCell>
+                                <TableCell align="left">{prefix}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell align="left">First Name</TableCell>
+                                <TableCell align="left">{firstName}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell align="left">Last Name</TableCell>
+                                <TableCell align="left">{lastName}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell align="left">Middle Name</TableCell>
+                                <TableCell align="left">{middleName}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell align="left">Suffix</TableCell>
+                                <TableCell align="left">{suffix}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell align="left">Gender</TableCell>
+                                <TableCell align="left">{sex}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell align="left">Date of Birth</TableCell>
+                                <TableCell align="left">{moment(dateOfBirth).format('DD-MMMM-YYYY')}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell align="left">Place of Birth</TableCell>
+                                <TableCell align="left">{placeOfBirth}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell align="left">Address</TableCell>
+                                <TableCell align="left">{address}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell align="left">Apt./Suite</TableCell>
+                                <TableCell align="left">{aptSuite}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell align="left">City</TableCell>
+                                <TableCell align="left">{city}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell align="left">State/Province</TableCell>
+                                <TableCell align="left">{stateProvince}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell align="left">Postal Code</TableCell>
+                                <TableCell align="left">{postalCode}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell align="left">Email</TableCell>
+                                <TableCell align="left">{emailAddress}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell align="left">Reference Credential Type</TableCell>
+                                <TableCell align="left">{credentialType}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell align="left">Credential ID Number</TableCell>
+                                <TableCell align="left">{credentialNumber}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell align="left">Date of Issue</TableCell>
+                                <TableCell align="left">{moment(dateOfIssue).format('DD-MMMM-YYYY')}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell align="left">Date of Expiration</TableCell>
+                                <TableCell align="left">{moment(dateOfExpiration).format('DD-MMMM-YYYY')}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell align="left">Issuing Authority</TableCell>
+                                <TableCell align="left">{issuingAuthority}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell align="left">Feedback Medium</TableCell>
+                                <TableCell align="left">{sendMedium}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell align="left">Discount Offer</TableCell>
+                                <TableCell align="left">{discountOffer?"YES":"NO"}</TableCell>
+                            </TableRow>
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </Fragment>
+    )
+}
+
+export default SummaryPage
+
+
+/*
+  <Fragment>
             <Typography variant="h6" gutterBottom align="center">
                 {stepTitle[activeStep]}
             </Typography>
@@ -52,7 +178,5 @@ const SummaryPage = ({stepTitle,activeStep}) => {
                 </Table>
             </TableContainer>
         </Fragment>
-    )
-}
 
-export default SummaryPage
+*/
